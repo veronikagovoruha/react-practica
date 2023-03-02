90f1c1c86b312dbf8023e79d910b0fceba077d82
Veronika <veronikagovoruha@gmail.com>
2023-03-02 16:23:22 +0200

    finish module 2.2

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -2,25 +2,47 @@ import TransactionForm from "../TransactionForm/TransactionForm";
 import CategoriesList from "../CategoriesList/CategoriesList";
 import Header from "../Header/Header";
 import s from "./MainPage.module.css"
+import { Component } from "react";
 
-const MainPage = ({onIncomesBtnClick, onCostsBtnClick}) => {
-    const isCategoryList = false;
 
-    return (
-        <div className="container">
-           <Header 
-            title={isCategoryList ? 'Категорії' : 'Журнал витрат'}
-            icon = {isCategoryList ? '#icon-left' : null}
-           />
-            {isCategoryList ? <CategoriesList /> : 
-            <>
-            <TransactionForm/>
-            <div className={s.blockBtn}>
-                <button className={s.costs} onClick = {onIncomesBtnClick}>Всі витрати</button>
-                <button className={s.incomes} onClick = {onCostsBtnClick}>Всі прибутки</button>
+class MainPage  extends Component{
+
+    state = {
+        isCategoryList: false
+    };
+
+    handelOpenCategoriesList = () => {
+        this.setState({isCategoryList: true})
+    }
+
+    handelCloseCategoriesList = () => {
+        this.setState({isCategoryList: false})
+    }
+
+    render(){
+        const {onIncomesBtnClick, onCostsBtnClick} = this.props;
+        const {isCategoryList} = this.state;
+        return (
+            <div className="container">
+               <Header 
+                title={isCategoryList ? 'Категорії' : 'Журнал витрат'}
+                icon = {isCategoryList ? '#icon-left' : null}
+                cbOnClick={this.handelCloseCategoriesList}
+               />
+               <main className={s.main}>
+               {isCategoryList ? <CategoriesList /> : 
+                <>
+                <TransactionForm handelOpenCategoriesList={this.handelOpenCategoriesList}/>
+                <div className={s.blockBtn}>
+                    <button className={s.costs} onClick = {onIncomesBtnClick}>Всі витрати</button>
+                    <button className={s.incomes} onClick = {onCostsBtnClick}>Всі прибутки</button>
+                </div>
+                </>}
+               </main>
             </div>
-            </>}
-        </div>
-    )
+        )
+    }
+
+
 }
 export default MainPage;
--- src/components/TransactionForm/TransactionForm.jsx
+++ src/components/TransactionForm/TransactionForm.jsx
@@ -1,30 +1,58 @@
+import { Component } from "react";
 import s from "./TransactionForm.module.css";
 
-const TransactionForm = () => {
-    return(
-        <>
-        <form className={s.form}>
-                <label className={s.label}>День
-                    <input className={s.inputDate} type="date" name="date"/>
-                </label>
-                <label className={s.label}>Час 
-                    <input className={s.inputTime} type="time" name="time"/>
-                </label>
-                <label className={s.label}>Категорія 
-                    <input className={s.inputCategory}  type="button" name="category" value={'Продукти'}/>
-                </label>
-                <label className={s.label}>Сума 
-                    <input className={s.inputSum} type="text" name="sum" placeholder="Введіть суму"/>
-                </label>
-                <label className={s.label}l>Валюта 
-                    <input className={s.inputCurrency} type="button" name="currency" value={'UAH'}/>
-                </label>
-                <label className={s.label}>
-                    <input className={s.inputcomment} type="text" name="comment" placeholder="Коментар"/>
-                </label>
-            </form>
-        </>
-    )
+class TransactionForm extends Component {
+    state = {
+        date: '2023-03-02',
+        time: '13:00',
+        category: 'Продукти',
+        sum: '',
+        currency: 'UAH',
+        comment: ''
+    }
+
+    handlerChange = (e) => {
+        const {name, value} = e.target;
+        this.setState({[name]: value})
+    }
+
+    render(){
+        const {handelOpenCategoriesList} = this.props;
+        const {date, time, category, sum, currency, comment} = this.state;
+        return(
+            <>
+            <form className={s.form}>
+                    <label className={s.label}>День
+                        <input className={s.inputDate} type="date" name="date" onChange={this.handlerChange} value={date}/>
+                    </label>
+                    <label className={s.label}>Час 
+                        <input 
+                        className={s.inputTime} 
+                        type="time" name="time" 
+                        onChange={this.handlerChange} 
+                        value={time}/>
+                    </label>
+                    <label className={s.label}>Категорія 
+                        <input 
+                        className={s.inputCategory} 
+                        onClick={handelOpenCategoriesList}  
+                        type="button" 
+                        name="category" 
+                        value={category}/>
+                    </label>
+                    <label className={s.label}>Сума 
+                        <input className={s.inputSum} type="text" name="sum" value={sum} onChange={this.handlerChange} placeholder="Введіть суму"/>
+                    </label>
+                    <label className={s.label}l>Валюта 
+                        <input className={s.inputCurrency} type="button" name="currency" onChange={this.handlerChange} value={currency}/>
+                    </label>
+                    <label className={s.label}>
+                        <input className={s.inputcomment} type="text" name="comment" onChange={this.handlerChange} value={comment} placeholder="Коментар"/>
+                    </label>
+                </form>
+            </>
+        )
+        }
 }
 
 export default TransactionForm;
